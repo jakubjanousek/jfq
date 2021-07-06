aa8eadf1703c2d3d5a9c1ac5f510dff5eefece2b
jakub.janousek@gmail.com <jakub.janousek@gmail.com>
2021-07-06 08:18:49 +0200

    Fix date format

--- components/Date.tsx
+++ components/Date.tsx
@@ -3,9 +3,14 @@ import { parseISO, format } from "date-fns";
 type Props = { dateString: string; displayTime?: boolean };
 
 const Date: React.FC<Props> = ({ dateString, displayTime }) => {
-  const dateFormat = displayTime ? "d.M.yyyy - HH:mm" : "d.M.yyyy";
+  const dateFormat = displayTime ? "d.M.yyyy HH:mmh" : "d.M.yyyy";
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, dateFormat)}</time>;
+  const formattedDate = format(date, "d.M.yyyy");
+  const formattedTime = format(date, "HH:mm");
+  const output = displayTime
+    ? `${formattedDate} o ${formattedTime} h`
+    : formattedDate;
+  return <time dateTime={dateString}>{output}</time>;
 };
 
 export default Date;
