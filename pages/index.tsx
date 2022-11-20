0be5b2c36386f20afcfb5080ce45f823e2957bac
jakubjanousek <jakub.janousek@gmail.com>
2022-11-20 05:57:46 +0100

    Remove banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,18 +39,6 @@ const Section: React.FC<{ bg: string }> = ({ bg, children }) => (
 const Home: React.FC<Props> = ({ posts, shows, invites, longForms }) => {
   return (
     <Layout>
-      <a href="/hudba/v-buducom-zivote">
-        <div className="mb-2 mx-auto max-w-2xl text-center font-serif font-bold italic text-2xl">
-          <div className="m-auto mb-4" style={{ maxWidth: "200px" }}>
-            <img src="obchod/v-buducom.jpg" width="800" height="733" />
-          </div>
-          Nové CD V budúcom živote vyšlo v októbri 2021
-        </div>
-        <Divider />
-        <strong className="text-center block underline">Zisti viac...</strong>
-      </a>
-      <div className="mb-16" />
-
       <div className="flex flex-wrap -m-4">
         <Section bg="bg2">
           <HomeSubHeading>Novinky</HomeSubHeading>
